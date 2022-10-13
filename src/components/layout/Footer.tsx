fbd55ab1520ed7fb06175338ea61a9fe55a2adfb
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 14:21:08 -0600

    style: footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,28 @@
+import {
+  Center,
+  Container,
+  Flex,
+  Heading,
+} from '@chakra-ui/react'
+
 function Footer() {
   return (
-    <div data-cy="content-footer">Footer</div>
+    <Container maxW="container.xl">
+      <Flex
+        data-cy="content-footer"
+        h={50}
+      >
+        <Center>
+          <Heading
+            as="h4"
+            size={'sm'}
+            color="white"
+          >
+            Copyright 2022
+          </Heading>
+        </Center>
+      </Flex>
+    </Container>
   )
 }
 
