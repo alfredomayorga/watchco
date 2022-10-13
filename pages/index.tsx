5302a9e9a7b933ca9a723a0f5b5705af817f1d9d
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 17:06:47 -0600

    feat: implement featured section in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,29 @@ import type { NextPage } from 'next'
 import Content from '../src/components/layout/Content'
 // eslint-disable-next-line
 import HomeHeroSection from '@/components/ui/HomeHeroSection'
-import { HeroContent } from 'src/typings/common.types'
+import {
+  FeaturedItem,
+  HeroContent,
+} from 'src/typings/common.types'
+import FeatureSection from '@/components/ui/FeaturedSection'
 
 interface HomeProps {
   heroContent: HeroContent
+  featuredItems: FeaturedItem[]
 }
 
 const Home: NextPage<HomeProps> = (props) => {
-  const { heroContent } = props
+  const { heroContent, featuredItems } = props
   return (
     <Content>
-      <HomeHeroSection
-        heroContent={heroContent}
-      />
+      <>
+        <HomeHeroSection
+          heroContent={heroContent}
+        />
+        <FeatureSection
+          featuredItems={featuredItems}
+        />
+      </>
     </Content>
   )
 }
