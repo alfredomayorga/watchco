50dd5f6d582dfae115106340c5a4f75bf93f30e9
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 17:06:30 -0600

    feat: featured section component

---
+++ src/components/ui/FeaturedSection/index.tsx
@@ -0,0 +1,32 @@
+import {
+  Box,
+  Flex,
+  Heading,
+} from '@chakra-ui/react'
+import React from 'react'
+// eslint-disable-next-line
+import type { FeaturedSectionProps } from '../../../typings/common.interfaces'
+import FeaturedItem from './FeaturedItem'
+function FeatureSection({
+  featuredItems,
+}: FeaturedSectionProps) {
+  return (
+    <Box>
+      <Heading mb={8}>Featured</Heading>
+      <Flex
+        gap={'24px'}
+        mb={'48px'}
+        data-cy="featured-section"
+      >
+        {featuredItems.map((item) => (
+          <FeaturedItem
+            name={item.name}
+            imageUrl={item.imageUrl}
+          />
+        ))}
+      </Flex>
+    </Box>
+  )
+}
+
+export default FeatureSection
