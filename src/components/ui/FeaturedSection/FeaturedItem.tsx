a746e6e343f42b220248fe44daf2f93e35b2a57c
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 17:06:14 -0600

    feat: featured item component

---
+++ src/components/ui/FeaturedSection/FeaturedItem.tsx
@@ -0,0 +1,50 @@
+import {
+  Center,
+  Flex,
+  Heading,
+} from '@chakra-ui/react'
+import Image from 'next/future/image'
+import { FeaturedItemType } from 'src/typings/common.types'
+
+function FeaturedItem({
+  name,
+  imageUrl,
+}: FeaturedItemType) {
+  return (
+    <Flex
+      flex="1"
+      h={'100%'}
+      direction="column"
+      overflow={'hidden'}
+    >
+      <Flex
+        h={250}
+        overflow={'hidden'}
+        borderRadius={10}
+      >
+        <Center>
+          <Image
+            src={imageUrl}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+            alt={name}
+          />
+        </Center>
+      </Flex>
+      <Heading
+        as="h6"
+        size={'md'}
+        mt={4}
+      >
+        {name}
+      </Heading>
+    </Flex>
+  )
+}
+
+export default FeaturedItem
