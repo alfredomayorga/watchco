3d8a98dcf52b59048d299865ea28ed091a8691a4
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 10:11:02 -0600

    test: create initial layout testing

---
+++ cypress/e2e/layout.cy.ts
@@ -0,0 +1,43 @@
+describe('Test layout', () => {
+  before(() => {
+    cy.visit('/')
+  })
+  it('has a content container', () => {
+    cy.getByData('content-container').should(
+      'exist'
+    )
+  })
+  it('should have a top bar', () => {
+    cy.getByData('content-top-bar').should(
+      'exist'
+    )
+  })
+  it('has the company logo', () => {
+    cy.getByData('company-logo').should('exist')
+  })
+  it('should have a user avatar', () => {
+    cy.getByData('user-avatar').should('exist')
+  })
+  it('should have a navigation menu', () => {
+    cy.getByData('navigation').should('exist')
+  })
+  it('needs to have a link to the home page', () => {
+    cy.getByData('home-link').should(
+      'have.attr',
+      'href',
+      '/'
+    )
+  })
+  it('needs to have a link to the shop page', () => {
+    cy.getByData('shop-link').should(
+      'have.attr',
+      'href',
+      '/shop'
+    )
+  })
+  it('should have a button to view cart', () => {
+    cy.getByData('view-cart')
+  })
+})
+
+export {}
