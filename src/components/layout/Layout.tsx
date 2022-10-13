b864970aeb7065f406682d8d06e7cafc3a60ac5b
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 12:09:42 -0600

    feat: implement logo and user avatar in nav

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,25 @@ import Topbar from './Topbar'
 import Footer from './Footer'
 // eslint-disable-next-line
 import type { LayoutProps } from '../../typings/common.interfaces'
+import {
+  Container,
+  Divider,
+} from '@chakra-ui/react'
+
 function Layout({ children }: LayoutProps) {
   return (
     <>
-      <Topbar />
-      {children}
-      <Footer />
+      <Container maxW="container.xl">
+        <Topbar />
+      </Container>
+
+      <Divider />
+      <Container maxW="container.xl">
+        {children}
+      </Container>
+      <Container maxW="container.xl">
+        <Footer />
+      </Container>
     </>
   )
 }
--- src/components/layout/Topbar.tsx
+++ src/components/layout/Topbar.tsx
@@ -1,6 +1,25 @@
+import {
+  Flex,
+  Center,
+  Spacer,
+} from '@chakra-ui/react'
+import CompanyLogo from '../shared/CompanyLogo/CompanyLogo'
+import UserAvatar from '../shared/UserAvatar/UserAvatar'
 function Topbar() {
   return (
-    <nav data-cy="content-top-bar">Topbar</nav>
+    <Flex
+      data-cy="content-top-bar"
+      direction={'row'}
+      h={65}
+    >
+      <Center>
+        <CompanyLogo />
+      </Center>
+      <Spacer />
+      <Center>
+        <UserAvatar />
+      </Center>
+    </Flex>
   )
 }
 
