0d6cf1687c50426f4002ab042f2a72e49b14c76d
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 16:20:15 -0600

    feat: implement hero section on homepage
    Use getStaticProps for SSR with DB to get content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
+import heroData from '../db/hero-content.json'
+
 import type { NextPage } from 'next'
 import Content from '../src/components/layout/Content'
+// eslint-disable-next-line
+import HomeHeroSection from '@/components/ui/HomeHeroSection'
+import { HeroContent } from 'src/typings/common.types'
 
-const Home: NextPage = () => {
+interface HomeProps {
+  heroContent: HeroContent
+}
+
+const Home: NextPage<HomeProps> = (props) => {
+  const { heroContent } = props
   return (
     <Content>
-      <h1>Watch Co.</h1>
+      <HomeHeroSection
+        heroContent={heroContent}
+      />
     </Content>
   )
 }
 
 export default Home
+
+export async function getStaticProps() {
+  return {
+    props: heroData,
+  }
+}
