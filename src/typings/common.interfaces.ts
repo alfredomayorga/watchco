06cb1bf2bea4777222aa9ca0e7bc04b4cc9ea00b
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-13 16:17:54 -0600

    feat: create types and interfaces for homepage

--- src/typings/common.interfaces.ts
+++ src/typings/common.interfaces.ts
@@ -1,3 +1,15 @@
+/* eslint-disable */
+
 export interface LayoutProps {
   children: JSX.Element
+}
+
+export interface HeroImageProps {
+  name: string
+  imageUrl: string
+}
+
+export interface HeroTextProps {
+  heading: string
+  content: string
 }
---
+++ src/typings/common.types.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+export type HeroContent = {
+  heroImage: string
+  heroHeading: string
+  heroText: string
+}
+
+export type HomepageContent = {
+  heroContent: HeroContent
+}
