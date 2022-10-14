e3af2bb15b761a9b5f20360b7f70f5f7e2b588bf
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 09:38:40 -0600

    feat: implement item overlay on categories

--- src/components/ui/CategorySection/CategoryLink.tsx
+++ src/components/ui/CategorySection/CategoryLink.tsx
@@ -1,17 +1,29 @@
+import ItemOverlay from '@/components/shared/ItemOverlay'
 import { Box } from '@chakra-ui/react'
 import Image from 'next/future/image'
+import { useState } from 'react'
 import { FeaturedItemType } from 'src/typings/common.types'
 
 function CategoryLink({
   imageUrl,
 }: FeaturedItemType) {
+  const [showOverlay, setShowOverlay] =
+    useState<boolean>(false)
+
+  function toggleOverlay() {
+    setShowOverlay(!showOverlay)
+  }
   return (
     <Box
+      position="relative"
       flex="1"
       h={400}
       overflow="hidden"
       borderRadius={'10px'}
+      onMouseEnter={toggleOverlay}
+      onMouseLeave={toggleOverlay}
     >
+      {showOverlay && <ItemOverlay />}
       <Image
         src={imageUrl}
         alt="Category"
