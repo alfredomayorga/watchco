e6c6f2ca0c2b6a3b1e36011cd924e05a60c999b5
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 10:38:33 -0600

    feat: implement category pages routing

--- src/components/ui/CategorySection/index.tsx
+++ src/components/ui/CategorySection/index.tsx
@@ -3,6 +3,7 @@ import {
   Flex,
   Heading,
 } from '@chakra-ui/react'
+import Link from 'next/link'
 // eslint-disable-next-line
 import { CategorySectionProps } from 'src/typings/common.interfaces'
 import CategoryLink from './CategoryLink'
@@ -19,21 +20,33 @@ function CategorySection({
         flex="1"
         data-cy="mens-section"
       >
-        <Heading mb={4}>Men&apos;s</Heading>
-        <CategoryLink
-          name={categorySection[0].name}
-          imageUrl={categorySection[0].imageUrl}
-        />
+        <Link href="/shop/mens-watches">
+          <a>
+            <Heading mb={4}>Men&apos;s</Heading>
+            <CategoryLink
+              name={categorySection[0].name}
+              imageUrl={
+                categorySection[0].imageUrl
+              }
+            />
+          </a>
+        </Link>
       </Box>
       <Box
         flex="1"
         data-cy="womens-section"
       >
-        <Heading mb={4}>Women&apos;s</Heading>
-        <CategoryLink
-          name={categorySection[1].name}
-          imageUrl={categorySection[1].imageUrl}
-        />
+        <Link href="/shop/womens-watches">
+          <a>
+            <Heading mb={4}>Women&apos;s</Heading>
+            <CategoryLink
+              name={categorySection[1].name}
+              imageUrl={
+                categorySection[1].imageUrl
+              }
+            />
+          </a>
+        </Link>
       </Box>
     </Flex>
   )
