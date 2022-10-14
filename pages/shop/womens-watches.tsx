cd9b3aabb9b6f0ce54c9ba9572872c7885cf8667
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 12:08:10 -0600

    feat: implement womens watches page

--- pages/shop/womens-watches.tsx
+++ pages/shop/womens-watches.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
-
-function WomensPage() {
-  return <div>Women&apos;s Page</div>
+import WatchBoxList from '@/components/shared/WatchBoxList'
+import { NextPage } from 'next'
+import womensWatches from '../../db/womens-watches.json'
+/* eslint-disable-next-line max-len */
+import type { CategoryProps } from '../../src/typings/common.interfaces'
+const WomensPage: NextPage<CategoryProps> = (
+  props
+) => {
+  return (
+    <WatchBoxList watchBoxes={props.watchBoxes} />
+  )
 }
 
 export default WomensPage
+
+export async function getStaticProps() {
+  return {
+    props: womensWatches,
+  }
+}
