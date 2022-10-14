8e45bd98a1e128952002ff962ebe53cbc1506b10
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 10:29:13 -0600

    feat: implement shop page

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import heroData from '../db/hero-content.json'
 
-function shop() {
-  return <div>shop</div>
+// eslint-disable-next-line max-len
+import CategorySection from '@/components/ui/CategorySection'
+import React from 'react'
+import { NextPage } from 'next'
+import { HomeProps } from 'src/typings/common.interfaces'
+import { Box, SlideFade } from '@chakra-ui/react'
+import FeatureSection from '@/components/ui/FeaturedSection'
+
+const ShopPage: NextPage<HomeProps> = (props) => {
+  const { categorySection, featuredItems } = props
+  return (
+    <Box mt={8}>
+      <SlideFade in={true}>
+        <CategorySection
+          categorySection={categorySection}
+        />
+      </SlideFade>
+      <FeatureSection
+        featuredItems={featuredItems}
+      />
+    </Box>
+  )
 }
 
-export default shop
+export default ShopPage
+
+export async function getStaticProps() {
+  return {
+    props: heroData,
+  }
+}
