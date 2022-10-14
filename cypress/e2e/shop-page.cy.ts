7341fcd127b3be713771f4674e440f602605fb8c
alfredomayorga <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 10:28:48 -0600

    test: create shop pages testing

--- cypress/e2e/shop-page.cy.ts
+++ cypress/e2e/shop-page.cy.ts
@@ -2,7 +2,16 @@ describe('Shop page', () => {
   before(() => {
     cy.visit('/shop')
   })
-  it('should have 2 categories', () => {})
+  it('should have 2 categories on the main page', () => {
+    cy.getByData('category-section')
+      .children()
+      .should('have.length', 2)
+  })
+  it('should include the featured section', () => {
+    cy.getByData('featured-section').should(
+      'exist'
+    )
+  })
 })
 
 export {}
