7254a4fe765fb3564b371f26d54d126b7ad2c82a
Yehuda Zeytim <115187281+alfredomayorga@users.noreply.github.com>
2022-10-14 09:40:37 -0600

    Homepage link hover effects (#5)
    * feat: item overlay component
    
    * feat: implement featured item overlay
    
    * feat: implement item overlay on categories

--- src/components/ui/FeaturedSection/FeaturedItem.tsx
+++ src/components/ui/FeaturedSection/FeaturedItem.tsx
@@ -1,28 +1,40 @@
+import ItemOverlay from '@/components/shared/ItemOverlay'
 import {
   Center,
   Flex,
   Heading,
 } from '@chakra-ui/react'
 import Image from 'next/future/image'
+import { useState } from 'react'
 import { FeaturedItemType } from 'src/typings/common.types'
 
 function FeaturedItem({
   name,
   imageUrl,
 }: FeaturedItemType) {
+  const [showOverlay, setShowOverlay] =
+    useState<boolean>(false)
+
+  function toggleOverlay() {
+    setShowOverlay(!showOverlay)
+  }
   return (
     <Flex
       flex="1"
       h={'100%'}
       direction="column"
       overflow={'hidden'}
+      onMouseEnter={toggleOverlay}
+      onMouseLeave={toggleOverlay}
     >
       <Flex
         h={250}
         overflow={'hidden'}
         borderRadius={10}
       >
-        <Center>
+        <Center pos={'relative'}>
+          {showOverlay && <ItemOverlay />}
+
           <Image
             src={imageUrl}
             width={0}
